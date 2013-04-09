513afdd71774d6adc84ec8b6c9787cabae95ca2c
Jasper St. Pierre <jstpierre@mecheye.net>
2013-04-08 20:52:37 -0400

    Rewrite the export 3DS plugin to actually work

--- addons/export3ds.cpp
+++ addons/export3ds.cpp
@@ -41,42 +41,6 @@
 //objects need smoothend normals and which not. so level export
 //needs some manual tweaking
 
-struct Vert
-{
-  Vert(int a, int b, int c)
-    : posIndex(a), tex0Index(b), matIndex(c)
-  {
-  }
-
-  int posIndex;
-  int tex0Index;
-  int matIndex;
-
-  bool operator<(const Vert& b) const
-  {
-    return memcmp(this, &b, sizeof(Vert)) < 0;
-  }
-};
-
-typedef std::map<Vert, int> PairMap;
-
-static int
-getIndex(const Index& index, const Attributes& attribs, const PairMap& pairMap)
-{
-  int posIndex = index.posIndex;
-  int texIndex = 0, matIndex = 0;
-  if(attribs.hasTexCoords[0])
-    texIndex = index.texCoordIndex[0];
-  if(attribs.hasMatrixIndices)
-    matIndex = index.matrixIndex;
-
-  PairMap::const_iterator it;
-  it = pairMap.find(Vert(posIndex, texIndex, matIndex));
-  assert(it != pairMap.end());
-
-  return it->second;
-}
-
 static int
 countTriangles(const Primitive& curr)
 {
@@ -111,11 +75,31 @@ countTriangles(const Batch& b)
   return triCount;
 }
 
-//it's better if one batch is one mesh, this makes hierarchy
-//handling easier
+struct Vert {
+  Index point;
+  int idx;
+};
+
+static int
+internVert(std::map<int, Vert> &usedVerts,
+           Index point)
+{
+  std::map<int, Vert>::iterator it = usedVerts.find(point.posIndex);
+
+  if (it == usedVerts.end())
+  {
+    Vert v;
+    v.point = point;
+    v.idx = usedVerts.size();
+    usedVerts[point.posIndex] = v;
+  }
+
+  return usedVerts[point.posIndex].idx;
+}
+
 static Lib3dsMesh*
 batchToMesh(const Batch& b, const BModel& bmd, const std::string& name,
-            int matIndex)
+            int material)
 {
   const Vtx1& vtx = bmd.vtx1;
   const Attributes& attribs = b.attribs;
@@ -123,135 +107,21 @@ batchToMesh(const Batch& b, const BModel& bmd, const std::string& name,
   if(!attribs.hasPositions)
     return NULL;
 
-  size_t i, j;
-
-  //count number of triangles
-  //(3ds doesn't support tristrips and trifans, so we have to convert
-  //everything to normal triangles)
-  int triCount = countTriangles(b);
-
-  //this is a bit tricky: bmd files store a position index
-  //and a tex coord index for each point. 3ds has only
-  //one index that is used as index for both the position array
-  //and the tex coords array. So we have to check how many
-  //(posIndex, texCoords0Index) pairs are in the bmd file,
-  //and use that many "unified" indices in the 3ds file.
-
-  //count needed vertices/texcoords
-  PairMap pairMap; //holds (posIndex, texCoords0Index) pairs
-
-  //decides which normals are smoothend
-  std::map<int, std::set<int> > normalMapSet;
-
-  for(j = 0; j < b.packets.size(); ++j)
-  {
-    const Packet& p = b.packets[j];
-    for(i = 0; i < p.primitives.size(); ++i)
-    {
-      const Primitive& curr = p.primitives[i];
-      for(size_t k = 0; k < curr.points.size(); ++k)
-      {
-        int texIndex = 0, matIndex = 0;
-
-        if(attribs.hasTexCoords[0])
-          texIndex = curr.points[k].texCoordIndex[0];
-
-        if(attribs.hasMatrixIndices)
-          matIndex = curr.points[k].matrixIndex;
-
-        if(attribs.hasNormals)
-          normalMapSet[curr.points[k].posIndex].insert(curr.points[k].normalIndex);
-
-        pairMap[Vert(curr.points[k].posIndex, texIndex, matIndex)] = -1;
-      }
-    }
-  }
-
-  //copy data
   Lib3dsMesh* mesh = lib3ds_mesh_new(name.c_str());
 
-  //vertices and texcoords
-  lib3ds_mesh_resize_vertices(mesh, pairMap.size(), 1, 0);
+  int triCount = countTriangles(b);
 
-  PairMap::iterator it, end = pairMap.end();
-  for(i = 0, it = pairMap.begin(); it != end; ++it, ++i)
-  {
-    Vert curr = it->first;
-
-    mesh->vertices[i][0] = vtx.positions[curr.posIndex].x();
-    mesh->vertices[i][1] = vtx.positions[curr.posIndex].y();
-    mesh->vertices[i][2] = vtx.positions[curr.posIndex].z();
-
-    if(attribs.hasTexCoords[0])
-    {
-      float u = vtx.texCoords[0][curr.tex0Index].s;
-      float v = vtx.texCoords[0][curr.tex0Index].t;
-
-      //store v flipped - 3ds max has v texcoord axis inverted
-      mesh->texcos[i][0] = u;
-      mesh->texcos[i][1] = 1.f - v;
-    }
-
-    it->second = i;
-  }
-
-  //transform positions to global space
+  std::map<int, Vert> usedVerts;
   Matrix44f matrixTable[10];
-  std::set<Vert> alreadyTransformed;
-  for(j = 0; j < b.packets.size(); ++j)
-  {
-    const Packet& p = b.packets[j];
-
-    updateMatrixTable(bmd, p, matrixTable);
-    Matrix44f mat = matrixTable[0];
-
-    for(i = 0; i < p.primitives.size(); ++i)
-    {
-      const Primitive& curr = p.primitives[i];
-      for(size_t j = 0; j < curr.points.size(); ++j)
-      {
-        int tex0Index = 0, matIndex = 0;
-        if(attribs.hasTexCoords[0])
-          tex0Index = curr.points[j].texCoordIndex[0];
-        if(attribs.hasMatrixIndices)
-          matIndex = curr.points[j].matrixIndex;
-
-        if(alreadyTransformed.find(Vert(curr.points[j].posIndex,
-          tex0Index, matIndex)) != alreadyTransformed.end())
-          continue;
-        alreadyTransformed.insert(Vert(curr.points[j].posIndex,
-          tex0Index, matIndex));
-
-        if(attribs.hasMatrixIndices)
-          mat = matrixTable[curr.points[j].matrixIndex/3];
-        //mat = Matrix44f::IDENTITY;
-
-        //bmd has x right, y up, z in
-        //3ds has x right, z up, y out
-        //so swap y and z, change sign of y
-        int index = getIndex(curr.points[j], attribs, pairMap);
-
-        Vector3f v(mesh->vertices[index][0],
-                   mesh->vertices[index][1],
-                   mesh->vertices[index][2]);
-        v = mat*v;
-        mesh->vertices[index][0] = v.x();
-        mesh->vertices[index][1] = -v.z();
-        mesh->vertices[index][2] = v.y();
-      }
-    }
-  }
-
 
   //tris
   lib3ds_mesh_resize_faces(mesh, triCount);
   int triIndex = 0;
+  size_t i, j, k;
+
   for(j = 0; j < b.packets.size(); ++j)
   {
     const Packet& p = b.packets[j];
-
-    //set up matrix table
-    updateMatrixTable(bmd, p, matrixTable);
 
     for(i = 0; i < p.primitives.size(); ++i)
     {
@@ -262,40 +132,16 @@ batchToMesh(const Batch& b, const BModel& bmd, const std::string& name,
       int b = 1;
 
       bool flip = true;
-      for(size_t k = 2; k < curr.points.size(); ++k)
+      for(k = 2; k < curr.points.size(); ++k)
       {
         int c = k;
 
-        //set all edges visibility to true
-        mesh->faces[triIndex].flags = LIB3DS_FACE_VIS_AC | LIB3DS_FACE_VIS_BC | LIB3DS_FACE_VIS_AB;
+        mesh->faces[triIndex].index[0] = internVert(usedVerts, curr.points[a]);
+        mesh->faces[triIndex].index[1] = internVert(usedVerts, curr.points[b]);
+        mesh->faces[triIndex].index[2] = internVert(usedVerts, curr.points[c]);
 
-        //get "unified" index
-        int ia, ib, ic;
-        ia = mesh->faces[triIndex].index[0] =
-          getIndex(curr.points[a], attribs, pairMap);
-        ib = mesh->faces[triIndex].index[1] =
-          getIndex(curr.points[b], attribs, pairMap);
-        ic = mesh->faces[triIndex].index[2] =
-          getIndex(curr.points[c], attribs, pairMap);
-
-        //if at least one vertex of the current triangle has more than
-        //one normal, the triangle contains a "hard" edge, don't put
-        //it into a smoothing group. Otherwise, smooth it.
-
-        //this didn't work well with levels, which contain no normal data,
-        //but might have antiparallel triangles whose normals would add up
-        //zero - so use this only if normals were present.
-
-        //*
-        if(attribs.hasNormals
-          && !(normalMapSet[ia].size() > 1 || normalMapSet[ib].size() > 1
-               || normalMapSet[ic].size() > 1))
-          mesh->faces[triIndex].smoothing_group = 1; // XXX -- bad?
-        //*/
-
-        //set material name
         if(attribs.hasTexCoords[0])
-          mesh->faces[triIndex].material = matIndex;
+          mesh->faces[triIndex].material = material;
 
         if(flip)
           std::swap(mesh->faces[triIndex].index[2],
@@ -313,6 +159,50 @@ batchToMesh(const Batch& b, const BModel& bmd, const std::string& name,
           case GX_TRIANGLE_FAN:
             b = c;
             break;
+        }
+      }
+    }
+  }
+
+  lib3ds_mesh_resize_vertices(mesh, usedVerts.size(), 1, 0);
+
+  for(j = 0; j < b.packets.size(); ++j)
+  {
+    const Packet& p = b.packets[j];
+
+    updateMatrixTable(bmd, p, matrixTable);
+
+    for(i = 0; i < p.primitives.size(); ++i)
+    {
+      const Primitive& curr = p.primitives[i];
+      for(k = 0; k < curr.points.size(); ++k)
+      {
+        const Index &point = curr.points[k];
+
+        int idx = usedVerts[point.posIndex].idx;
+
+        Vector3f v = vtx.positions[point.posIndex];
+
+        Matrix44f mat = matrixTable[0];
+        if(attribs.hasMatrixIndices)
+          mat = matrixTable[point.matrixIndex/3];
+        else
+          mat = Matrix44f::IDENTITY;
+
+        v = mat * v;
+
+        mesh->vertices[idx][0] = v.x();
+        mesh->vertices[idx][1] = -v.z();
+        mesh->vertices[idx][2] = v.y();
+
+        if(attribs.hasTexCoords[0])
+        {
+          float u = vtx.texCoords[0][point.texCoordIndex[0]].s;
+          float v = vtx.texCoords[0][point.texCoordIndex[0]].t;
+
+          // store v flipped - 3ds max has v texcoord axis inverted
+          mesh->texcos[idx][0] = u;
+          mesh->texcos[idx][1] = 1.f - v;
         }
       }
     }
